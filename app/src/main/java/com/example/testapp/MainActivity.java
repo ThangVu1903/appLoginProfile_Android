dd14a280f0b1cb0aed9259e90ab492345493c515
ThangVu1903 <vuthang19032002az@gmail.com>
2022-08-27 20:22:55 +0700

    full project app basic

--- app/src/main/java/com/example/testapp/MainActivity.java
+++ app/src/main/java/com/example/testapp/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.testapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button btnLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.LoginMe);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openScreenProfile();
+            }
+        });
+    }
+    public void openScreenProfile(){
+        Intent intent = new Intent(this, ScreenProfile.class);
+        startActivity(intent);
     }
 }
